45970db05391a851d8e23a7e2e44e2f20ed42d06
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-07-06 19:56:35 +0530

    Added basic crud repo and like-repo code

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,6 +1,10 @@
 import Tweet from "../models/tweet.js";
+import CrudRepository from "./crud-repository.js";
 
-class TweetRepository {
+class TweetRepository extends CrudRepository {
+  constructor(){
+    super(Tweet)
+  }
   async create(data) {
     try {
       const tweet = await Tweet.create(data);
@@ -10,14 +14,7 @@ class TweetRepository {
     }
   }
 
-  async get(id) {
-    try {
-      const tweet = await Tweet.findById(id);
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
+
 
   async update(tweetId, data) {
     try {
@@ -30,14 +27,7 @@ class TweetRepository {
     }
   }
 
-  async destroy(id) {
-    try {
-      const tweet = await Tweet.findByIdAndRemove(id);
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
+
 
   async getWithComments(id) {
     try {
